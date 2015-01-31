7cea921ad52f8a4793eb7341d05044349f578b6f
Igor Manushin <igor.manushin@optimarea.com>
2015-01-31 18:27:07 +0300

    +safe type case

--- CheckContracts/Properties/AssemblyInfo.cs
+++ CheckContracts/Properties/AssemblyInfo.cs
@@ -6,5 +6,5 @@
 [assembly: AssemblyProduct("CheckContracts")]
 [assembly: AssemblyCopyright("Copyright © 2015")]
 
-[assembly: AssemblyVersion("1.2.0.0")]
-[assembly: AssemblyFileVersion("1.2.0.0")]
+[assembly: AssemblyVersion("1.3.0.0")]
+[assembly: AssemblyFileVersion("1.3.0.0")]
