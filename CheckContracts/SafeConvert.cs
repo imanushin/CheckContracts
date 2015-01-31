e55284cd42eb5fa6393fa17a3ab773c0bfdd5373
Igor Manushin <igor.manushin@optimarea.com>
2015-01-31 18:28:21 +0300

    extended tags

---
+++ CheckContracts/SafeConvert.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using CheckContracts.Properties;
+
+namespace CheckContracts
+{
+    /// <summary>
+    /// Do convertions with detailed exceptions
+    /// </summary>
+    public static class SafeConvert
+    {
+        /// <summary>
+        /// Cast input value to the target type. It can be useful during implementing <typeparamref name="IValueConverter"/> interface.
+        /// Method raises ArgumentNullException in case of null argument.
+        /// </summary>
+        [ContractAnnotation("input:null => halt")]
+        public static TResult ConvertArgument<TResult>(object input, [InvokerParameterName] string argumentName)
+            where TResult : class
+        {
+            Validate.ArgumentIsNotNull(input, argumentName);
+
+            var result = input as TResult;
+
+            Validate.ArgumentCondition(
+                !ReferenceEquals(null, result),
+                argumentName,
+                "Invalid argument {0} with type {1}: unable to cast it to the {2}",
+                argumentName,
+                input.GetType().Name,
+                typeof(TResult).Name);
+
+            return (TResult)input;
+        }
+
+        /// <summary>
+        /// Cast input value to the target type. It can be useful during implementing <typeparamref name="IValueConverter"/> interface.
+        /// Method raises InvalidOperationException in case of <paramref name="input"/>.
+        /// </summary>
+        [ContractAnnotation("input:null => halt")]
+        [StringFormatMethod("messageFormat")]
+        public static TResult Convert<TResult>([NotNull] object input, string messageFormat, params object[] arguments)
+            where TResult : class
+        {
+            Validate.IsNotNull(input, messageFormat, arguments);
+
+            var result = input as TResult;
+
+            Validate.Condition(!ReferenceEquals(null, result), messageFormat, arguments);
+
+            return (TResult)input;
+        }
+    }
+}
