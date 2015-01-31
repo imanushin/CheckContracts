623a5b94c6ce4ee3c19ef6767f40382322f983ea
Igor Manushin <igor.manushin@optimarea.com>
2015-01-31 17:53:20 +0300

    Code fix

---
+++ NuGet/InstallTests/SomeNetProject/SomeFile.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CheckContracts;
+
+namespace SomeNetProject
+{
+    internal static class SomeFile
+    {
+        public static void SomeMethod(string str)
+        {
+            Validate.ArgumentStringIsMeanful(str, "str");
+        }
+    }
+}
